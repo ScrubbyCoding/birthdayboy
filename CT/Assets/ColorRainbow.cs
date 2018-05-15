2d2bffaf51cb80b2a151ded437241f901f27462e
Matthew <matthewmeans8@gmail.com>
2018-05-14 17:03:57 -0700

    kyans ting
    i added the boi

---
+++ CT/Assets/ColorRainbow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ColorRainbow : MonoBehaviour {
+
+    public float Speed = 1;
+    private Renderer rend;
+    private Text Text;
+    public Material Base;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        Text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        Base.SetColor("_Color", HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * Speed, 1), 1, 1)));
+        Text.color = Base.color;
+    }
+}
---
+++ CT/Assets/FlyIn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyIn : MonoBehaviour {
+    public float Speed;
+    public bool lerpstop;
+    public GameObject Continue;
+    public float WaitTime;
+    public AudioSource Slam;
+    public bool CanContinue;
+    public GameObject Main;
+    public GameObject Selection;
+    public AudioSource Con;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Stop());
+	}
+	IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(.4f);
+        Slam.Play();
+        yield return new WaitForSeconds(WaitTime);
+        lerpstop = true;
+        Continue.SetActive(true);
+        CanContinue = true;
+        
+    }
+	// Update is called once per frame
+	void Update () {
+        if (CanContinue)
+        {
+            if (Input.anyKeyDown)
+            {
+                Con.Play();
+                Main.SetActive(false);
+                Selection.SetActive(true);
+                CanContinue = false;
+                
+            }
+        }
+        if (!lerpstop)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.7f, 0.7f, 0.7f), Speed);
+        }
+	}
+}
